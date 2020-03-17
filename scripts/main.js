cac38984d650b143f5fc9f4341aecbf9ff4624e2
Bindu <bindubharuka@yahoo.com>
2020-03-17 15:52:57 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,7 @@
 const myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
