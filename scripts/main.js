98ffc803cf7463ff4bd28b7c8ad59108314ea4d6
Bindu <bindubharuka@yahoo.com>
2020-03-17 16:10:13 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,11 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+/**const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';**/
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
 const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
+function random(min,max) {
+  const num = Math.floor(Math.random()*(max-min)) + min;
+  return num;
+}
